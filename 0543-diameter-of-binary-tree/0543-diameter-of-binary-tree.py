e2b702026090d35da90b07233a6902ef7f5c77e7
Siddharth Sinha <33250458+sidsinha16@users.noreply.github.com>
2025-05-15 00:00:26 +0530

    Time : 0(nlogn) SC : o(1){java}

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -0,0 +1,14 @@
+class Solution:
+    def diameterOfBinaryTree(self, root):
+        self.max_diameter = 0
+
+        def depth(node):
+            if not node:
+                return 0
+            left = depth(node.left)
+            right = depth(node.right)
+            self.max_diameter = max(self.max_diameter, left + right)
+            return 1 + max(left, right)
+
+        depth(root)
+        return self.max_diameter
