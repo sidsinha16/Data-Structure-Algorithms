dc251e824c4deee9666d09875c084a40e70cb1ef
Siddharth Sinha <33250458+sidsinha16@users.noreply.github.com>
2025-06-18 23:29:27 +0530

    Time : 0(nlogn) SC : o(1){java}

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+        
+        for (ListNode node : lists) {
+            if (node != null) pq.add(node);
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode min = pq.poll();
+            tail.next = min;
+            tail = tail.next;
+
+            if (min.next != null) {
+                pq.add(min.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
