f628fb3f16bb592fde1dc2dcf337a09b8b82aefb
Siddharth Sinha <33250458+sidsinha16@users.noreply.github.com>
2025-06-30 22:29:30 +0530

    Time : 0(nlogn) SC : o(1){java}

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -0,0 +1,11 @@
+public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    
+    for (int num : nums) {
+        minHeap.offer(num);
+        if (minHeap.size() > k) {
+            minHeap.poll();  // Remove smallest to keep size k
+        }
+    }
+    return minHeap.peek();  // kth largest
+}
