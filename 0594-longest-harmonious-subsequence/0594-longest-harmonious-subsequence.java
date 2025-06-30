4fe19aea98b79990da5c429d4fc36bbbfcf29aa9
Siddharth Sinha <33250458+sidsinha16@users.noreply.github.com>
2025-06-30 22:24:52 +0530

    Time : 0(nlogn) SC : o(1){java}

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        for(int key : map.keySet()){
+            if(map.containsKey(key+1)){
+                result = Math.max(result, map.get(key)+map.get(key+1));
+            }
+        }
+        return result;
+    }
+}
