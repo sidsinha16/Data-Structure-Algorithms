515ece1b80f0b162dd6e1be2e94fb8fe39cb0779
Siddharth Sinha <33250458+sidsinha16@users.noreply.github.com>
2025-07-03 22:57:58 +0530

    Time : 0(nlogn) SC : o(1){java}

---
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findKthCharacter(int k) {
+    StringBuilder word = new StringBuilder("a");
+
+    while (word.length() < k) {
+        StringBuilder next = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            char nextChar = (char)((c - 'a' + 1) % 26 + 'a');
+            next.append(nextChar);
+        }
+        word.append(next);
+    }
+
+    return word.charAt(k - 1);
+}
+
+}
