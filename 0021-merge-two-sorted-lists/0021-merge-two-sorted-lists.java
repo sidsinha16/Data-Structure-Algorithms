153f4fc59e0ea5d37685ce495bab5e46cad8e467
Siddharth Sinha <33250458+sidsinha16@users.noreply.github.com>
2025-07-16 23:24:09 +0530

    Time : 0(nlogn) SC : o(1){java}

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -14,26 +14,25 @@ class Solution {
         ListNode temp = dummy;
 
         while(list1 != null && list2 != null){
-           
-           if(list1.val <= list2.val){
+            if(list1.val <= list2.val){
                 temp.next = list1;
                 list1 = list1.next;
-                //list1.next = temp.next;
-           }else{
+                temp = temp.next;
+            }else{
                 temp.next = list2;
                 list2 = list2.next;
-                //list2.next = temp.next;
-           }
-           temp = temp.next;
+                temp = temp.next;
+            }
         }
 
         if(list1 != null){
             temp.next = list1;
         }
+
         if(list2 != null){
             temp.next = list2;
         }
-        
+
         return dummy.next;
     }
 }
