4143ed56fc78fc1007a6bd658fdeae4109074cd5
Siddharth Sinha <33250458+sidsinha16@users.noreply.github.com>
2025-07-16 17:30:51 +0530

    Time : 0(nlogn) SC : o(1){java}

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -13,12 +13,15 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
+
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
+
             if(slow == fast){
                 slow = head;
                 while(slow != fast){
+                    System.out.print(slow.val);
                     slow = slow.next;
                     fast = fast.next;
                 }
