fd33421e36be062d881ab8005c6343e536f307a1
Siddharth Sinha <33250458+sidsinha16@users.noreply.github.com>
2025-07-29 23:36:21 +0530

    Time : 0(nlogn) SC : o(1){java}

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -4,11 +4,20 @@ class Solution {
         int[] result = new int[n];
         Stack<Integer> stack = new Stack<>(); // store indices
 
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int prevIndex = stack.pop();
-                result[prevIndex] = i - prevIndex;
+        // for (int i = 0; i < n; i++) {
+        //     while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+        //         int prevIndex = stack.pop();
+        //         result[prevIndex] = i - prevIndex;
+        //     }
+        //     stack.push(i);
+        // }
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
             }
+
+            result[i] = stack.isEmpty() ? 0 : stack.peek() - i;
             stack.push(i);
         }
 
